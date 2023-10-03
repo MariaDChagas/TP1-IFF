ee8ca06ed6a14a39c2d187bedcd95821f6f10565
MariaDChagas <m.chagas@gsuite.iff.edu.br>
2023-10-03 17:07:08 -0300

    a

---
+++ Aula 4/formulario.js
@@ -0,0 +1,24 @@
+const PORT = 8080;
+const express = require("express");
+const app = express();
+const fs = require('fs');
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('cadastro')
+})
+
+app.post('/app',(req, res) => {
+ const data = {
+   nome: req.body.nome,
+   sobrenome: req.body.sobrenome
+ }
+ res.render('exemplo', {data})
+})
+
+app.listen(PORT, () => {
+console.log(`server funcionando na porta ${PORT}`)
+})
